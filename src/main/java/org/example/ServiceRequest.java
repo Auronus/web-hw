bc18fe2514c7336747b4b9108ce05a6c2607318c
Auronus <auronus@mail.ru>
2024-10-04 12:26:13 +0300

    First iteration of query parsing

--- src/main/java/org/example/ServiceRequest.java
+++ src/main/java/org/example/ServiceRequest.java
@@ -1,14 +1,15 @@
 package org.example;
 
-import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import org.apache.http.client.utils.URLEncodedUtils;
+
+import java.io.*;
 import java.net.Socket;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.LocalDateTime;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 public class ServiceRequest implements Runnable {
     private final List<String> validPaths = List.of("/index.html", "/spring.svg", "/spring.png", "/resources.html", "/styles.css", "/app.js", "/links.html", "/forms.html", "/classic.html", "/events.html", "/events.js");
@@ -21,29 +22,51 @@ public class ServiceRequest implements Runnable {
     //@Override
     public void run() {
         try (
-                final BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                final BufferedInputStream in = new BufferedInputStream(socket.getInputStream());
                 final BufferedOutputStream out = new BufferedOutputStream(socket.getOutputStream())
         ) {
-            final String requestLine = in.readLine();
-            final String[] parts = requestLine.split(" ");
+            // лимит на request line + заголовки
+            final var limit = 4096;
 
-            if (parts.length != 3) {
+            in.mark(limit);
+            final byte[] buffer = new byte[limit];
+            final var read = in.read(buffer);
+            // ищем request line
+            final var requestLineDelimiter = new byte[]{'\r', '\n'};
+            final var requestLineEnd = indexOf(buffer, requestLineDelimiter, 0, read);
+            if (requestLineEnd == -1) {
+                badRequest(out);
                 socket.close();
                 return;
             }
 
-            final String path = parts[1];
-            if (!validPaths.contains(path)) {
+            // читаем request line
+            final String[] requestLine = new String(Arrays.copyOf(buffer, requestLineEnd)).split(" ");
+            if (requestLine.length != 3) {
+                badRequest(out);
+                socket.close();
+                return;
+            }
+
+            Request request = new Request(requestLine[0], requestLine[1], requestLine[2]);
+            System.out.println(request);
+            for (Map.Entry<String, String> pair: request.getQueryParams().entrySet()) {
+                System.out.println(pair.getKey() + "=" + pair.getValue());
+            }
+            System.out.println(request.getQueryParam("title"));
+            System.out.println(request.getQueryParam("value"));
+            System.out.println(request.getQueryParam("image"));
+            if (!validPaths.contains(request.getPath())) {
                 notFoundOut(out);
                 socket.close();
                 return;
             }
 
-            final Path filePath = Path.of("public", path);
+            final Path filePath = Path.of("public", request.getPath());
             final String mimeType = Files.probeContentType(filePath);
 
             // special case for classic
-            if (path.equals("/classic.html")) {
+            if (request.getPath().equals("/classic.html")) {
                 classicCaseOut(out, filePath, mimeType);
                 socket.close();
                 return;
@@ -72,6 +95,16 @@ public class ServiceRequest implements Runnable {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private static void badRequest(BufferedOutputStream out) throws IOException {
+        out.write((
+                "HTTP/1.1 400 Bad Request\r\n" +
+                        "Content-Length: 0\r\n" +
+                        "Connection: close\r\n" +
+                        "\r\n"
+        ).getBytes());
+        out.flush();
     }
 
     private void classicCaseOut(BufferedOutputStream out, Path filePath, String mimeType) {
@@ -110,5 +143,18 @@ public class ServiceRequest implements Runnable {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+    // from google guava with modifications
+    private static int indexOf(byte[] array, byte[] target, int start, int max) {
+        outer:
+        for (int i = start; i < max - target.length + 1; i++) {
+            for (int j = 0; j < target.length; j++) {
+                if (array[i + j] != target[j]) {
+                    continue outer;
+                }
+            }
+            return i;
+        }
+        return -1;
     }
 }
